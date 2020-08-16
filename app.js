0f07ec6a74aad429ce30e94abca722236f2b202b
rahulganeshani <69670304+rahulganeshani@users.noreply.github.com>
2020-08-17 00:14:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyCq4XNmFp5ll2yiOsztLqB9JViW0cWThQU",
+  authDomain: "hacking-3c9af.firebaseapp.com",
+  databaseURL: "https://hacking-3c9af.firebaseio.com",
+  projectId: "hacking-3c9af",
+  storageBucket: "hacking-3c9af.appspot.com",
+  messagingSenderId: "690240191132",
+  appId: "1:690240191132:web:4b574749b33e5ac8430b59",
+  measurementId: "G-DJG8WMT20Y"
 };
 
      
